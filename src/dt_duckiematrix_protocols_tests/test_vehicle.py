0b0b86547aa09b95a74c38ef69cafed724aa400e
Andrea F. Daniele <afdaniele@ttic.edu>
2022-06-10 10:22:19 -0500

    minor

--- src/dt_duckiematrix_protocols_tests/test_vehicle.py
+++ src/dt_duckiematrix_protocols_tests/test_vehicle.py
@@ -2,21 +2,16 @@ import time
 
 from dt_duckiematrix_protocols import Matrix
 
-matrix = Matrix(
-    "localhost",
-    auto_commit=True
-)
+matrix = Matrix("localhost", auto_commit=True)
 
-time.sleep(1)
-
-robot = matrix.robots.DB21M("vautobot00", raw_pose=True)
+robot = matrix.robots.DB21M("map_0/vehicle_0", raw_pose=True)
 
 speed = 1.0
-drive_for_num_steps = 0
+drive_for_num_steps = 100
 
 for _ in range(drive_for_num_steps):
     print(_)
     speed += 0.0
     with robot.session():
-        robot.drive(-speed, speed)
-    time.sleep(1 / 60.0)
+        robot.drive(speed, speed)
+    time.sleep(1 / 10.0)
