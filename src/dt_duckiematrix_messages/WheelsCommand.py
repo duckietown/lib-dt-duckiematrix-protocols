3a43b6c380044b0a35d3d5856a2e1b7ef9028967
davide <Tuxliri@users.noreply.github.com>
2023-06-15 17:17:40 -0500

    Added pwm wheels command

--- src/dt_duckiematrix_messages/WheelsCommand.py
+++ src/dt_duckiematrix_messages/WheelsCommand.py
@@ -8,3 +8,7 @@ from dt_duckiematrix_messages import CBorMessage
 @dataclasses.dataclass
 class WheelsCommand(CBorMessage):
     wheels: Dict[str, float]
+
+@dataclasses.dataclass
+class PWMWheelsCommand(CBorMessage):
+    wheels: Dict[str, float]
