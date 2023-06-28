454d0f6cc5f56a4c8554ba30d5796342c27debbe
davide <Tuxliri@users.noreply.github.com>
2023-06-28 15:40:00 -0500

    Implemented initial structure of the env

--- src/gym_duckiematrix/envs/duckiebot.py
+++ src/gym_duckiematrix/envs/duckiebot.py
@@ -1,13 +1,19 @@
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
+from dt_duckiematrix_protocols import Matrix
 
 class DuckietownEnv(gym.Env):
     
-    def __init__(self, gain=1.0, trim=0.0, radius=0.0318, k=27.0, limit=1.0, **kwargs):
+    def __init__(self, gain=1.0, trim=0.0, radius=0.0318, k=27.0, limit=1.0, matrix_hostname : str = "localhost", **kwargs):
         
         self.action_space = spaces.Box(low=np.array([-1, -1]), high=np.array([1, 1]), dtype=np.float32)
-    
+        # create connection to the matrix engine
+        matrix = Matrix(matrix_hostname, auto_commit=True)
+        # get a list of robots we want to control and check that they are all the robots present in the map
+        # create connection to the vehicle
+        self.robot = matrix.robots.DB21M("map_0/vehicle_0")
+        
         # Should be adjusted so that the effective speed of the robot is 0.2 m/s
         self.gain = gain
 
@@ -24,6 +30,7 @@ class DuckietownEnv(gym.Env):
         self.limit = limit
 
     def step(self, action):
+        # make action a dictionary containing actions for each duckiebot
         pass
     
     def reset(self):
