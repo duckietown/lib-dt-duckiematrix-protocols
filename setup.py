32f85e431e3bcda91b8f328936e5b6b14c955731
Liam Paull <paulll@iro.umontreal.ca>
2025-03-29 09:31:53 -0400

    adding pyturbojpeg

--- setup.py
+++ setup.py
@@ -38,7 +38,9 @@ version = get_version_from_source('src/dt_duckiematrix_protocols/__init__.py')
 # with open(dependencies_file, 'rt') as fin:
 #     dependencies = list(filter(lambda line: not line.startswith('#'), fin.read().splitlines()))
 
-install_requires = []
+install_requires = [
+    "pyturbojpeg",
+]
 tests_require = []
 
 # compile description
